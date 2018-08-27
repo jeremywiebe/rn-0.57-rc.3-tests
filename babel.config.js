67eb6d3544acc9e1d26ad08058b08c976e10ac35
Jeremy Wiebe <jeremy@khanacademy.org>
2018-08-27 14:59:05 -0700

    Jest tests work with a transformer specified

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(api) {
-  api.cache(true);
-
-  return {
-    "plugins": ["@babel/plugin-transform-classes", "@babel/plugin-proposal-class-properties"],
-    "presets": ["module:metro-react-native-babel-preset"]
-  }
-}
